4d5d4401d2a2b0ba3897e2017f9a8b57ae3a2e29
bushra-n <160867479+bushra-n@users.noreply.github.com>
2024-05-18 17:19:54 +0500

    Add files via upload

---
+++ task25.ts
@@ -0,0 +1,26 @@
+// task 25 Aliens color ,passing version
+let alien_color:string = "Green"
+if (alien_color == "Green" ){
+    console.log("Congratulations! you have earned five points")
+}
+else {console.log("you have earned no point")}
+// fail version
+ alien_color ="Red"
+if (alien_color == "Green") {
+    console.log("Congratulations! you have earned five points")
+}
+else{
+    console.log("{}")
+}
+
+
+
+
+
+
+
+
+
+
+
+
